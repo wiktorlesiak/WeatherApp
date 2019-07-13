9930ad94fae82b8f23090b8a3a9c6e6f831937d9
wiktorlesiak <B00087701@student.itb.ie>
2019-07-13 20:00:41 +0100

    added buttons in main activity

---
+++ app/src/main/java/weather/app/CurrentWeather.java
@@ -0,0 +1,13 @@
+package weather.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CurrentWeather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_weather);
+    }
+}
--- app/src/main/java/weather/app/MainActivity.java
+++ app/src/main/java/weather/app/MainActivity.java
@@ -1,13 +1,38 @@
 package weather.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button currentButton, forecastButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentButton = (Button)findViewById(R.id.currentButton);
+        forecastButton = (Button)findViewById(R.id.forecastButton);
+
+        currentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CurrentWeather.class));
+
+            }
+        });
+
+        forecastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
     }
 }
